6d517efa4c69b982ff570b01b768912df64b5970
beelarr <bryonl@me.com>
2018-02-28 17:37:40 -0600

    Add container with primary color '

---
+++ app/components/Container/Container.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./styles";
+
+import { View } from "react-native";
+
+const Container = children => <View style={styles.container}>{children}</View>;
+
+
+
+export default Container;
