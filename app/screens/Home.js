e114997e8182ad86f4906f69dace5a4509e47a23
beelarr <bryonl@me.com>
2018-03-05 13:47:55 -0600

    Add funtions for buttons

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,34 +1,40 @@
 import React, { Component } from "react";
 import { StatusBar } from "react-native";
 import { Container } from "../components/Container";
-import { Logo } from '../components/Logo'
-import { InputWithButton } from '../components/TextInput/InputWithButton';
+import { Logo } from "../components/Logo";
+import { InputWithButton } from "../components/TextInput/InputWithButton";
 
-
-const TEMP_BASE_CURRENCY = 'USD';
-const TEMP_QUOTE_CURRENCY = 'GBP';
-const TEMP_BASE_PRICE = '100';
-const TEMP_QUOTE_PRICE = '79.74';
-
+const TEMP_BASE_CURRENCY = "USD";
+const TEMP_QUOTE_CURRENCY = "GBP";
+const TEMP_BASE_PRICE = "100";
+const TEMP_QUOTE_PRICE = "79.74";
 
 class Home extends Component {
+  handlePressBaseCurrency = () => {
+    console.log("press base");
+  };
+
+  handlePressQuoteCurrency = () => {
+    console.log("press quote");
+  };
+
   render() {
-      return (
-	      <Container>
-		      <StatusBar translucent={false} barStyle="light-content" />
-		      <Logo />
-		      <InputWithButton
-			      buttonText={}
-			      onPress={}
-		      />
-		      <InputWithButton
-			      buttonText={}
-			      onPress={}
-			      editable={false}
-		      />
-	      </Container>
-      )
+    return (
+      <Container>
+        <StatusBar translucent={false} barStyle="light-content" />
+        <Logo />
+        <InputWithButton
+          buttonText={TEMP_BASE_CURRENCY}
+          onPress={this.handlePressBaseCurrency}
+        />
+        <InputWithButton
+          buttonText={TEMP_QUOTE_CURRENCY}
+          onPress={this.handlePressQuoteCurrency}
+          editable={false}
+        />
+      </Container>
+    );
   }
-};
+}
 
 export default Home;
